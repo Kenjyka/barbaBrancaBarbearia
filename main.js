9f91ab264f7cf48a874a2553980b50d724f68d53
Bruno <kendy.iuata@gmail.com>
2023-09-13 22:17:08 -0300

    finalizando dia 13/09

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 tituloLogo = Array.from(document.querySelectorAll('.titleLetters'))
 indexTituloLogo = tituloLogo.length - 1
 
+function sleep(ms) {
+  return new Promise(espere => setTimeout(espere, ms))
+}
+
+
 function brotaletra(letra) {
     setTimeout(() => {
-        console.log(letra)
         letra.style.opacity = 1
         indexTituloLogo -= 1
-        console.log(indexTituloLogo >= 0)
         if(indexTituloLogo >= 0){
-            console.log('iai')
           brotaletra(tituloLogo[indexTituloLogo])  
         } 
     }, 170);
@@ -21,6 +23,50 @@ tituloLogo.forEach(Element => {
 if(indexTituloLogo >= 0) {
   brotaletra(tituloLogo[indexTituloLogo])  
 }
+
+//função dos promocionais
+
+const promocionais = document.getElementById('promocionais')
+const promocionaisCards = [...promocionais.querySelectorAll('article')]
+let continuaCarouselCards = true
+let indexCardsPromocionais = 0
+let promocionaisCardsSaindoMouse = false
+rodaPromocionais()
+
+promocionaisCards.forEach((element, index) => {
+  element.addEventListener('mouseenter', () => {
+    (index == promocionaisCards.length -1) ? indexCardsPromocionais = 0 : indexCardsPromocionais = index + 1;
+    if (continuaCarouselCards == true) continuaCarouselCards = false;
+    limpaPromocionais()
+  })
+  element.addEventListener('mouseout', () => {
+    if (!promocionaisCardsSaindoMouse) {
+      promocionaisCardsSaindoMouse = true
+      if (continuaCarouselCards == false) continuaCarouselCards = true;
+      rodaPromocionais()
+      promocionaisCardsSaindoMouse = false
+    }
+  })
+})
+
+function limpaPromocionais () {
+  promocionaisCards.forEach(element => {
+    if (element.classList.contains('active')) element.classList.remove('active');
+  })
+}
+
+function rodaPromocionais () {
+  if (continuaCarouselCards) {
+      limpaPromocionais()
+      promocionaisCards[indexCardsPromocionais].classList.add('active');
+      (indexCardsPromocionais == promocionaisCards.length - 1) ? indexCardsPromocionais = 0 : indexCardsPromocionais++;
+  }
+}
+
+setInterval(() => {
+  rodaPromocionais()
+}, 5000);
+
 
 
 
---
+++ readme.md
@@ -0,0 +1,17 @@
+##  Barba Branca Barbearia
+Um projeto de um site para uma barbearia não-existente com o intuito de demonstrar as capacidades de HTML, CSS e Javascript.
+
+---
+
+### Lista de afazeres
+- [x] animação tesoura
+- [x] nome da empresa fade-in
+- [x] criar logo
+- [x] criar header
+- [x] criar carrossel
+- [x] editar imagens para o carrossel
+- [x] criar e automatizar os cards promocionais
+- [ ] links de artigos
+- [ ] plano de fundo dos links de artigos
+- [ ] aba de comentário sobre os artigos
+- [ ] footer
