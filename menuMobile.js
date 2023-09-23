10199d3299b05cc5a92b788dee31134d09fade78
Bruno <kendy.iuata@gmail.com>
2023-09-23 18:38:50 -0300

    melhorando o nav mobile

--- menuMobile.js
+++ menuMobile.js
@@ -8,3 +8,25 @@ hamburguerMobile.addEventListener("click", ()=> {
 sairMobile.addEventListener("click", ()=> {
   linksMobile.classList.toggle("active")
 })
+
+const mobileArrow = [...linksMobile.querySelectorAll(".material-symbols-sharp")] 
+
+mobileArrow.forEach(arrow => {
+  arrow.addEventListener("click", () => {
+    arrow.classList.toggle("rotated")
+    console.log(arrow)
+    let alvo = arrow.parentElement.parentElement.querySelector("ul")
+    alvo.classList.toggle("active")
+    mobileArrow.forEach(element => {
+      console.log(element.parentElement)
+      let alvoMenor = element.parentElement.parentElement.querySelector("ul")
+      if (alvoMenor.classList.contains("active") && alvoMenor != alvo) {
+        alvoMenor.classList.remove("active")
+      }
+      if (element.classList.contains("rotated" && element != arrow)) {
+        element.classList.remove("rotated")
+      }
+    })
+    
+  })
+})
