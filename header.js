ec477baba570372f6d61bd45c6755485a8cc526c
bruno <kendy.iuata@gmail.com>
2023-10-05 13:44:05 -0300

    perfilnav

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ const btnSair = document.querySelector("#sair")
 const btnCadastro = document.querySelector("#cadastro")
 const btnCadastroMobile = document.querySelector("#cadastro-mobile")
 const perfil = document.querySelectorAll(".perfil-nav")
-
+console.log(perfil)
 
 btnSair.addEventListener("click", () => {
   localStorage.setItem("isLoggedIn", JSON.stringify(false))
@@ -13,7 +13,16 @@ if(localStorage.getItem("isLoggedIn")  == "true") {
   btnCadastro.parentElement.classList.add("inactive")
   btnCadastroMobile.parentElement.classList.add("inactive")
   perfil.forEach(atual => {
-    atual.classList.remove("inactive")
     atual.classList.add("active")
   })
+} else {
+  if(btnCadastro.parentElement.classList.contains("inactive")) {
+    btnCadastro.parentElement.classList.remove("inactive")
+    btnCadastroMobile.parentElement.classList.remove("inactive")
+  }
+ perfil.forEach(atual => {
+  if (atual.classList.contains("active")) {
+    atual.classList.remove("active")
+  }
+ })
 }
